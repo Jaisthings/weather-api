18ad5ecc577bb522c6b490978271071972ba435d
jaisthings <jaisthings@outlook.com>
2020-12-24 22:10:55 +0800

    Fixed test case to pass it

--- src/test/java/com/jaisthings/weather/controllers/WeatherControllerTest.java
+++ src/test/java/com/jaisthings/weather/controllers/WeatherControllerTest.java
@@ -66,7 +66,7 @@ class WeatherControllerTest {
         ResponseEntity<CityWeather> responseEntity = weatherController.getCityWeather("");
 
         //then
-        assertThat(responseEntity.getBody()).isNotNull();
+        assertThat(responseEntity.getBody()).isNull();
         assertThat(responseEntity.getStatusCodeValue()).isEqualTo(500);
 
     }
